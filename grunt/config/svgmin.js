b1511d2f72a4f745eff6c65ddefcd11e336f5533
abusedmedia <ff@todo.to.it>
2017-07-21 17:39:47 +0200

    fix builder config

--- grunt/config/svgmin.js
+++ grunt/config/svgmin.js
@@ -1,34 +1,37 @@
-'use strict';
+'use strict'
 
-var path = require('path');
+var path = require('path')
 
 module.exports = function (grunt, options) {
-
   return {
 
     options: {
-        plugins: [
-            {removeViewBox: false}, 
-            {removeUselessStrokeAndFill: false}, 
+      plugins: [
+            {removeViewBox: false},
+            {removeUselessStrokeAndFill: false},
+            {removeUselessDefs: false},
+            {removeNonInheritableGroupAttrs: false},
+            {removeHiddenElems: false},
+            {removeEmptyContainers: false},
+            {removeEmptyText: false},
             {convertTransform: false},
-            {cleanupIDs:false},
-            {mergePaths:false},
-            {convertShapeToPath:false},
-            {moveElemsAttrsToGroup:false},
-            {moveGroupAttrsToElems:false}
-        ]
+            {cleanupIDs: false},
+            {mergePaths: false},
+            {convertShapeToPath: false},
+            {moveElemsAttrsToGroup: false},
+            {moveGroupAttrsToElems: false}
+      ]
     },
     dist: {
-        files: [
-        	{
-		        expand: true,
-		        cwd: options.folders.app,
-		        src: ['**/*.svg'],
-		        dest: options.folders.app
-		    }
-        ]
+      files: [
+        {
+          expand: true,
+          cwd: options.folders.app,
+          src: ['**/*.svg'],
+          dest: options.folders.app
+        }
+      ]
     }
 
-  };
-
-};
+  }
+}
